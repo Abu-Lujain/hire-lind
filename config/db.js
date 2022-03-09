b9bd76494546f59732ad2ebdbb921e6503e59a33
abu_lujain <codelog101@gmail.com>
2022-03-09 03:22:45 -0800

    set to deployment

--- config/db.js
+++ config/db.js
@@ -1,24 +1,21 @@
-const mongoose = require("mongoose");
- require("dotenv").config()
- const config = require("config")
- const express = require("express")
- const db = config.get("mongoURI")
- const connentDB = async () => {
-   try {
-     await mongoose.connect(
-       process.env.DB_Connetion_URI,
+require("dotenv").config()
+const mongoose = require("mongoose")
+const connentDB = async () => {
+  try {
+    await mongoose.connect(
+      process.env.DB_Connetion_URI,
 
-       {
-         useNewUrlParser: true,
-         useCreateIndex: true,
-         useUnifiedTopology: true,
-         useFindAndModify: false,
-       }
-     )
-     console.log("db connected")
-   } catch (error) {
-     console.log(error.message)
-     process.exit()
-   }
- }
-module.exports = connentDB;
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    )
+    console.log("db connected")
+  } catch (error) {
+    console.log(error.message)
+    process.exit()
+  }
+}
+module.exports = connentDB
