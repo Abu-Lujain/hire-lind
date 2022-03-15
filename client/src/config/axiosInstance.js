f2faa2f43fad646f0b45472c0f9d4f57bceb9bf6
abu_lujain <codelog101@gmail.com>
2022-03-15 05:53:25 -0700

    _

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:8000/api",
-  baseURL: "https://aamal.herokuapp.com/api/",
+  baseURL: "https://aamalonline.herokuapp.com/api/",
 })
-export const PF = "https://aamal.herokuapp.com"
+export const PF = "https://aamalonline.herokuapp.com"
 // export const PF = "http://localhost:8000"
