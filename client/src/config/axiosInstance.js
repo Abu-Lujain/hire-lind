24c1f420dd6a5fba224e32fd8336db02143dff02
abu_lujain <codelog101@gmail.com>
2022-03-15 05:19:50 -0700

    _

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
-  // baseURL: "https://carrerland.herokuapp.com/api/",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://carrerland.herokuapp.com/api/",
 })
-// export const PF = "https://carrerland.herokuapp.com"
-export const PF = "http://localhost:8000"
+export const PF = "https://carrerland.herokuapp.com"
+// export const PF = "http://localhost:8000"
